6178256cf875d6049db426447c135355f9879c82
Mauricio Valdora <mauricio_valdora@hotmail.com.ar>
2023-05-20 09:35:01 -0300

    proyecto avanzado

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let dolar = 460;
 let real = 44.21;
 let euro = 245.36;
@@ -7,6 +6,62 @@ let yenJapones = 1.62;
 let montoIngresada;
 let monedaIngresada;
 
+//usuarios existentes
+
+const usuarios = [
+    {   
+        'nombre':'Ariadna',
+        'apellido':'rindmond',
+        'contraseña':123,
+        'dinero':12341234
+    },
+    {
+        'nombre':'marisol',
+        'apellido':'maidana',
+        'contraseña':1243,
+        'dinero':123411234234
+    },
+    {
+        'nombre':'penelope',
+        'apellido':'estiba',
+        'contraseña':123,
+        'dinero':12341234
+    },
+    {
+        'nombre':'antonella',
+        'apellido':'legizamond',
+        'contraseña':12342134,
+        'dinero':12341234
+    }
+
+]
+
+class Usuario {
+    constructor(nombre, apellido, contraseña, dinero) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.contraseña = contraseña
+        this.dinero = dinero
+    }
+}
+
+//creacion de nuevo usuario
+
+let nombre = prompt('ingrese su nombre')
+let apellido = prompt('ingrese su apellido')
+let contraseña = prompt('ingrese su contraseña')
+let dinero = prompt('ingrese cantidad de dinero a depositar')
+const usuario = new Usuario(nombre, apellido, contraseña, dinero)
+usuarios.unshift(usuario)
+
+//Busqueda de algun usuario
+let usuarioBuscado = prompt('ingrese el nombre del usuario que busca')
+const usuarioEncontrado = usuarios.find(usuario=>usuario.nombre=usuarioBuscado)
+alert('El usuario que usted busca es: '+usuarioEncontrado.nombre +' '+ usuarioEncontrado.apellido)
+
+
+
+//convertidor de moneda
 function convertidorDeMoneda(monedaIngresada, montoIngresada) {
     switch (monedaIngresada) {
         case 'dolar':
@@ -31,10 +86,10 @@ function convertidorDeMoneda(monedaIngresada, montoIngresada) {
 
         default:
             return 'ingrese alguna moneda de las posibles'
-
     }
 }
-do {
+respuesta = prompt('¿quiere convertir alguna moneda?Si o no');
+while(respuesta=='si'){
     montoIngresada = prompt('Ingrese a el monto del dinero que quiere convertir');
     while (isNaN(montoIngresada) || montoIngresada < 0) {
         montoIngresada = prompt('Ingrese a el monto del dinero que quiere convertir nuevamente');
@@ -45,10 +100,12 @@ do {
         monedaIngresada = prompt('¿A que moneda quiere convertir? "Dolar" "Real" "Yen" "Euro" "Libras esterlinas"');
     }
     alert(convertidorDeMoneda(monedaIngresada, montoIngresada));
-    respuesta = prompt('¿quiere seguir convirtiendo?Si o no');
+    respuesta = prompt('Quiere convertir otra moneda?')
 
-} while (respuesta != 'no')
-
-
-
+}
+const user = document.getElementById('user')
+const boton = document.getElementById('boton');
+boton.addEventListener('click',()=>{
+    user.innerHTML='pronto estara disponible la opcion de registrar'
+})
 
