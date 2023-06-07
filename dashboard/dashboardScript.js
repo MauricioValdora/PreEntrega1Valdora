b14b06b76ade17e19737b4881f6f32aa8c382ffd
mauriciovaldora <mauricio_valdora@hotail.com.ar>
2023-06-07 00:24:56 -0300

    mejoreCodigo

--- dashboard/dashboardScript.js
+++ dashboard/dashboardScript.js
@@ -1,36 +1,164 @@
-let saldo = parseFloat(localStorage.getItem("dinero")) || 0; // Parsear el saldo como número y establecerlo en 0 si no existe
-
 let btn = document.getElementById('btn');
 let mensajeDeposito = document.getElementById("mensajeDeposito");
 let dineroDisponible = document.getElementById("dineroDisponible");
+let historialDepositos = document.getElementById("historialDepositos");
 
-document.addEventListener("DOMContentLoaded", function () {
-    var storedName = localStorage.getItem("nombre");
-    var welcomeMessageElement = document.getElementById("welcomeMessage");
-    welcomeMessageElement.textContent = "Hola, " + storedName + "!";
+const btnRetirar = document.getElementById('btnRetirar');
+const mensajeExtracciones = document.getElementById('mensajeExtracciones');
+const historialExtracciones = document.getElementById('historialExtracciones');
 
-    dineroDisponible.textContent = "Dinero disponible: $" + saldo;
-});
+// Obtener el historial de depósitos del Local Storage
+let extracciones = JSON.parse(localStorage.getItem("extracciones")) || [];
+let depositos = JSON.parse(localStorage.getItem("depositos")) || [];
+let saldo = parseFloat(localStorage.getItem("saldo")) || 0;
+
+// Función para mostrar el historial de depósitos
+function mostrarDepositos() {
+  historialDepositos.innerHTML = "";
+
+  for (let i = 0; i < depositos.length; i++) {
+    let deposito = depositos[i];
+    let depositoElement = document.createElement("p");
+    depositoElement.textContent = `Deposito N° ${i + 1}: $${deposito}`;
+    historialDepositos.appendChild(depositoElement);
+  }
+}
+
+
+
+function mostraExtracciones() {
+  historialExtracciones.innerHTML = "";
+
+  for (let i = 0; i < extracciones.length; i++) {
+    let extraccion = extracciones[i];
+    let extraccionElement = document.createElement("p");
+    extraccionElement.textContent = `Extraccion N° ${i + 1}: $${extraccion}`;
+    historialExtracciones.appendChild(extraccionElement);
+  }
+}
+
+// Mostrar el historial y saldo disponible al cargar la página
+mostrarDepositos();
+mostraExtracciones();
+dineroDisponible.textContent = "Dinero disponible: $" + saldo;
 
 btn.addEventListener('click', () => {
-    let dinero = document.getElementById('dineroDepositado').value;
+  let dinero = document.getElementById('dineroDepositado').value;
 
-    if (dinero !== '') {
-        mensajeDeposito.textContent = "Se depositaron $" + dinero;
-        document.getElementById("dineroDepositado").value = "";
+  if (dinero !== '') {
+    mensajeDeposito.textContent = "Se depositaron $" + dinero;
+    document.getElementById("dineroDepositado").value = "";
 
-        saldo += parseFloat(dinero); // Sumar el dinero depositado al saldo existente
+    // Agregar el depósito al historial
+    depositos.push(parseFloat(dinero));
 
-        // Actualizar el saldo en el Local Storage
-        localStorage.setItem("dinero", saldo);
+    // Actualizar el historial en el Local Storage
+    localStorage.setItem("depositos", JSON.stringify(depositos));
 
-        dineroDisponible.textContent = "Dinero disponible: $" + saldo;
-    }
+    mostrarDepositos();
+  }
 
-    setTimeout(function () {
-        mensajeDeposito.textContent = "";
-    }, 2000);
+  setTimeout(function () {
+    mensajeDeposito.textContent = "";
+  }, 2000);
 });
+
+// -----------------------------------------------------
+
+
+btnRetirar.addEventListener('click', ()=>{
+  const dineroRetirar = document.getElementById('dineroRetirado').value;
+
+  if (dineroRetirar !== '') {
+    mensajeExtracciones.textContent = "Se extrageron $" + dineroRetirar;
+    document.getElementById("dineroRetirado").value = "";
+
+    extracciones.push(parseFloat(dineroRetirar));
+
+    localStorage.setItem("extracciones", JSON.stringify( extracciones));
+    
+    mostraExtracciones()
+    
+  }
+  setTimeout(function () {
+    mensajeExtracciones.textContent = "";
+  }, 2000);
+  
+  
+})
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  function actualizarSaldo(){
+
+    const todosLosDepositos = depositos.reduce((a, b) => a + b, 0);
+    const todosLasExtracciones = extracciones.reduce((a, b) => a + b, 0);
+    const saldoFinal = todosLosDepositos - todosLasExtracciones;
+    localStorage.setItem("saldo", JSON.stringify(saldoFinal));
+    
+    dineroDisponible.textContent = "Dinero disponible: $" + saldoFinal;
+
+  }
+
+  setInterval(actualizarSaldo, 1000);
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
